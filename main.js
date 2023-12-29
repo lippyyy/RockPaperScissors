c6926512e16f21245e1067a60032b5ffd10ad265
lippyyy <111197353+lippyyy@users.noreply.github.com>
2023-12-29 19:16:46 +0000

    Complete code for playRound function

--- main.js
+++ main.js
@@ -18,7 +18,30 @@ function getUserChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    
+    let draw = true
+    while (draw == true) {
+        if (playerSelection == computerSelection) {
+            console.log("Draw! Go Again.")
+            playerSelection = getUserChoice()
+            computerSelection = getComputerChoice()
+        } if (playerSelection == "ROCK" && computerSelection == "PAPER") {
+            console.log("You Lose! Paper beats Rock.")
+        } else if (playerSelection == "ROCK" && computerSelection == "SCISSORS") {
+            console.log("You Win! Rock beats Scissors.")
+        } else if (playerSelection == "PAPER" && computerSelection == "ROCK") {
+            console.log("You Win! Paper beats Rock.")
+        } else if (playerSelection == "PAPER" && computerSelection == "SCISSORS") {
+            console.log("You Lose! Scissors beats Paper.")
+        } else if (playerSelection == "SCISSORS" && computerSelection == "ROCK") {
+            console.log("You Lose! Rock beats Scissors.")
+        } else if (playerSelection == "SCISSORS" && computerSelection == "PAPER") {
+            console.log("You Win! Scissors beats Paper.")
+        }
+
+        if (playerSelection != computerSelection) {
+            draw = false
+        }
+    }
 }
 
 playerSelection = getUserChoice();

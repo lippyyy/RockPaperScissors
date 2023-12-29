2efec01b26bf85536ee31ba6a3632e44f20c87d0
lippyyy <111197353+lippyyy@users.noreply.github.com>
2023-12-29 19:59:28 +0000

    Complete code for game function and add counters to playRound function

--- main.js
+++ main.js
@@ -18,32 +18,60 @@ function getUserChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    let draw = true
+    let draws = 0
+    let playerWins = 0
+    let computerWins = 0
+    let draw = true;
     while (draw == true) {
         if (playerSelection == computerSelection) {
-            console.log("Draw! Go Again.")
-            playerSelection = getUserChoice()
-            computerSelection = getComputerChoice()
+            console.log("Draw! Go Again.");
+            draws++;
+            playerSelection = getUserChoice();
+            computerSelection = getComputerChoice();
         } if (playerSelection == "ROCK" && computerSelection == "PAPER") {
-            console.log("You Lose! Paper beats Rock.")
+            console.log("You Lose! Paper beats Rock.");
+            computerWins++;
         } else if (playerSelection == "ROCK" && computerSelection == "SCISSORS") {
-            console.log("You Win! Rock beats Scissors.")
+            console.log("You Win! Rock beats Scissors.");
+            playerWins++;
         } else if (playerSelection == "PAPER" && computerSelection == "ROCK") {
-            console.log("You Win! Paper beats Rock.")
+            console.log("You Win! Paper beats Rock.");
+            playerWins++;
         } else if (playerSelection == "PAPER" && computerSelection == "SCISSORS") {
-            console.log("You Lose! Scissors beats Paper.")
+            console.log("You Lose! Scissors beats Paper.");
+            computerWins++;
         } else if (playerSelection == "SCISSORS" && computerSelection == "ROCK") {
-            console.log("You Lose! Rock beats Scissors.")
+            console.log("You Lose! Rock beats Scissors.");
+            computerWins++;
         } else if (playerSelection == "SCISSORS" && computerSelection == "PAPER") {
-            console.log("You Win! Scissors beats Paper.")
+            console.log("You Win! Scissors beats Paper.");
+            playerWins++;
         }
 
         if (playerSelection != computerSelection) {
-            draw = false
+            draw = false;
         }
     }
+    return [draws, playerWins, computerWins];
 }
 
-playerSelection = getUserChoice();
-computerSelection = getComputerChoice();
-playRound(playerSelection, computerSelection);
+function game() {
+    let draws = 0;
+    let wins = 0;
+    let losses = 0;
+    for (let i = 0; i < 5; i++) {
+        playerSelection = getUserChoice();
+        computerSelection = getComputerChoice();
+        let results = playRound(playerSelection, computerSelection);
+        
+        draws = draws + results[0]
+        wins = wins + results[1]
+        losses = losses + results[2]
+
+    console.log("Draws: ", draws);
+    console.log("Wins: ", wins);
+    console.log("Losses: ", losses);
+    }
+}   
+
+game()

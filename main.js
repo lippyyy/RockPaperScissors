7ed2a5aca5a34992dec9e7ff24abd70df81a78be
lippyyy <111197353+lippyyy@users.noreply.github.com>
2023-12-29 18:35:59 +0000

    Make user input case-insensitive

--- main.js
+++ main.js
@@ -2,17 +2,18 @@ function getComputerChoice() {
     let randomNum = Math.trunc(Math.random() * 3);
     let computerChoice;
     if (randomNum == 0) {
-        computerChoice = "Rock";
+        computerChoice = "ROCK";
     } else if (randomNum == 1) {
-        computerChoice = "Paper";
+        computerChoice = "PAPER";
     } else if (randomNum == 2) {
-        computerChoice = "Scissors";
+        computerChoice = "SCISSORS";
     }
     return computerChoice;
 }
 
 function getUserChoice() {
-    let userChoice = prompt("Rock, Paper, or Scissors?");
+    let userChoice = prompt("ROCK, PAPER, or SCISSORS?");
+    userChoice = userChoice.toUpperCase();
     return userChoice;
 }
 
